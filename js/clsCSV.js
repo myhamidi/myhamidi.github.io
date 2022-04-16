97374b884daf3db37fe9b200cf45c1450d645f69
myhamidi <myhamidi@hotmail.de>
2022-04-16 11:20:43 +0200

    rename csv->ecsv

--- js/clsCSV.js
+++ js/clsCSV.js
@@ -1,13 +1,16 @@
 
+// const myForm1 = document.getElementById("forminput1");
+// const myForm2 = document.getElementById("forminput2");
+
+// EASY CSV
+
 // ################################################################
 // HTML elements and Globals                                      #
 // ################################################################
 const reader = new FileReader();
 
-const myForm1 = document.getElementById("forminput1");
-const csvFile1 = document.getElementById("csvFile1");
-const myForm2 = document.getElementById("forminput2");
-const csvFile2 = document.getElementById("csvFile2");
+const ecsvFile1 = document.getElementById("ecsvFile1");
+const ecsvFile2 = document.getElementById("ecsvFile2");
 
 var csvText1 = ""; var csv1 = 0;
 var csvText2 = ""; var csv2 = 0;
@@ -17,14 +20,14 @@ var activeCSV = 0;
 // Event when loading file                                        #
 // ################################################################
 
-csvFile1.onchange = () => {
+ecsvFile1.onchange = () => {
   activeCSV = 1;
-  ReadFile(csvFile1.files[0])
+  ReadFile(ecsvFile1.files[0])
 }
 
-csvFile2.onchange = () => {
+ecsvFile2.onchange = () => {
   activeCSV = 2;
-  ReadFile(csvFile2.files[0])
+  ReadFile(ecsvFile2.files[0])
 }
 
 function ReadFile (file) {
@@ -38,14 +41,14 @@ function Load() {
   }
 
 function Loadend() {
-    eval("csv" + activeCSV + " = new myCSV(csvText" + activeCSV + ")")
+    eval("csv" + activeCSV + " = new clsCSV(csvText" + activeCSV + ")")
   }
 
 // ################################################################
 // class definition                                               #
 // ################################################################
 
-class myCSV {
+class clsCSV {
     constructor(csvtext, delimiter = ";") {
         var str = csvtext.replace(new RegExp('\r\n', "g") , '\n')
         this.headers = str.slice(0, str.indexOf("\n")).split(delimiter);
--- js/test/test_clsCSV.js
+++ js/test/test_clsCSV.js
@@ -8,18 +8,18 @@ function test_failed(fname) {
 
 class clsTest {
 
-    test_loadForms() {
-        name = "LoadForms"
-        var res = myForm1 == undefined || myForm2 == undefined
-        if (res) {
-            test_failed(name)}
-        else {
-            test_passed(name)}
-    }
+    // test_loadForms() {
+    //     name = "LoadForms"
+    //     var res = myForm1 == undefined || myForm2 == undefined
+    //     if (res) {
+    //         test_failed(name)}
+    //     else {
+    //         test_passed(name)}
+    // }
 
     test_CSVFile() {
         name = "CSVFile"
-        var res = csvFile1 == undefined || csvFile2 == undefined
+        var res = ecsvFile1 == undefined || ecsvFile2 == undefined
         if (res) {
             test_failed(name)}
         else {
@@ -37,7 +37,7 @@ class clsTest {
 
     test_LoadCSV2() {
         name = "LoadCSV 2"
-        var test_csv = new myCSV(csvText1)
+        var test_csv = new clsCSV(csvText1)
         var assert = IsEqual_List(test_csv.headers, ["Stadt", "Land", "Fluss"])
         if (assert) {
             test_passed(name)}
@@ -54,7 +54,7 @@ class clsTest {
 
 
 test = new clsTest()
-test.test_loadForms()
+// test.test_loadForms()
 test.test_CSVFile()
 test.test_LoadCSV()
 
